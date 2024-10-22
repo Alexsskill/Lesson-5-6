a413df36bc7d6a1b458329822feb9794fd42405f
Alexsskill <harckovaleks@yandex.ru>
2024-10-22 14:50:44 +0400

    homework lesson 29 30

---
+++ src/Homework2930/Book.java
@@ -0,0 +1,46 @@
+package Homework2930;
+
+public class Book {
+    // Поля для хранения данных о книге
+    private String title;   // Название книги
+    private Author author;  // Автор книги
+    private Integer year;   // Год публикации
+
+    // Сеттер для названия книги
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    // Сеттер для автора книги
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    // Сеттер для года публикации
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    // Геттер для названия книги
+    public String getTitle() {
+        return title;
+    }
+
+    // Геттер для автора книги
+    public Author getAuthor() {
+        return author;
+    }
+
+    // Геттер для года публикации
+    public Integer getYear() {
+        return year;
+    }
+
+    /**
+     * Метод, возвращающий название книги, её автора и год
+     * @return Название книги, имя автора и год издания книги
+     */
+    public String toString() {
+        return "Book: " + title + ", Author: " + author.getFullName() + ", Year: " + year;
+    }
+}

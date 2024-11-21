1217f1e1c7bcf1244f3358d616708710246436c9
Alexsskill <harckovaleks@yandex.ru>
2024-11-21 23:34:30 +0400

    homework lesson 41 42

--- src/Homework3738/Main.java
+++ src/Homework3738/Main.java
@@ -2,25 +2,35 @@ package Homework3738;
 
 public class Main {
     public static void main(String[] args) {
-        Player[] players = new Player[6];
 
-        // Создаем 6 игроков
-        for (int i = 0; i < 6; i++) {
-            players[i] = new Player();
-            players[i].info(); // Вывод информации о состоянии команд после каждого добавленного игрока
-        }
+        Game game = new Game();
 
-        // Пытаемся создать 7-го игрока и проверить результат
-        Player oneMorePlayer = new Player();
+        // Добавляем игроков в игру
+        game.addPlayer("Александр");
+        game.addPlayer("Василий");
+        game.addPlayer("Сергей");
+        game.addPlayer("Иван");
+        game.addPlayer("Дмитрий");
+        game.addPlayer("Владимир");
 
-        // Запускаем метод run() для каждого игрока, пока выносливость не достигнет нуля
-        for (Player player : players) {
-            while (player.getStamina() > 0) {
-                player.run(); // Игрок бегает, пока есть выносливость
+        // Пытаемся добавить еще одного игрока
+        game.addPlayer("Алексей"); // Не должно добавляться
+
+        // Выводим информацию о текущих игроках на поле
+        game.info();
+
+        // Запускаем метод run() для каждого игрока до полной истощенности
+        for (int i = 0; i < game.players.size(); i++) {
+            while (game.players.get(i).getStamina() > 0) {
+                game.players.get(i).run();
             }
         }
 
-        // Выводим окончательное количество игроков на поле
-        System.out.println("Количество игроков на поле: " + Player.getCountPlayers());
+        // Проверяем наличие игроков по имени
+        System.out.println("Игрок с именем 'Александр' есть в игре? " + game.hasPlayer("Александр"));
+        System.out.println("Игрок с именем 'Олег' есть в игре? " + game.hasPlayer("Олег"));
+
+        // Выводим окончательное количество игроков на поле после игры
+        game.info();
     }
 }
--- src/Homework3738/Player.java
+++ src/Homework3738/Player.java
@@ -5,19 +5,15 @@ import java.util.Random;
 public class Player {
 
     private int stamina;  // Выносливость
-    public static final int MAX_STAMINA = 10; // Максимальная выносливость
+    public static final int MAX_STAMINA = 100; // Максимальная выносливость
     public static final int MIN_STAMINA = 0;   // Минимальная выносливость
-    private static int countPlayers = 0;      // Количество игроков на поле
+    private String name;  // Имя игрока
 
     // Конструктор класса Player
-    public Player() {
+    public Player(String name) {
+        this.name = name;
         Random rand = new Random();
         this.stamina = rand.nextInt(11) + 90; // Генерация выносливости от 90 до 100
-        if (countPlayers < 6) {
-            countPlayers++; // Увеличиваем количество игроков, если игроков меньше 6
-        } else {
-            System.out.println("Нельзя создать больше игроков. Количество игроков на поле: " + countPlayers);
-        }
     }
 
     // Геттер для stamina
@@ -25,28 +21,17 @@ public class Player {
         return stamina;
     }
 
+    public String getName() {
+        return name;
+    }
+
     // Метод run(), уменьшающий выносливость и контролирующий уход игрока с поля
     public void run() {
         if (stamina > MIN_STAMINA) {
             stamina--; // Уменьшаем выносливость на 1
             if (stamina == MIN_STAMINA) {
-                countPlayers--; // Уменьшаем количество игроков, если игрок устал
-                System.out.println("Игрок устал и ушел с поля.");
+                System.out.println(name + " устал и ушел с поля.");
             }
         }
-    }
-
-    // Метод info()
-    public void info() {
-        if (countPlayers < 6) {
-            System.out.println("Команды неполные. На поле еще есть " + (6 - countPlayers) + " свободных мест");
-        } else {
-            System.out.println("На поле нет свободных мест");
-        }
-    }
-
-    // Метод для получения количества игроков на поле
-    public static int getCountPlayers() {
-        return countPlayers;
     }
 }
